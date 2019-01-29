7b8122dfa2ee8d25ae9c03964781ada77256bb0b
lucassousaf <lucas.sousa@magnet.coop>
2019-01-29 10:01:34 +0100

    Update clojure to 1.10.0, add deploy config and integration tests CI

--- project.clj
+++ project.clj
@@ -1,15 +1,24 @@
-(defproject magnet/object-storage.s3 "0.2.0"
+(defproject magnet/object-storage.s3 "0.3.0"
   :description "A Duct library for managing AWS S3 objects"
   :url "https://github.com/magnetcoop/object-storage.s3"
   :license {:name "Mozilla Public Licence 2.0"
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
-  :dependencies [[amazonica "0.3.136" :exclusions [com.amazonaws/aws-java-sdk
+  :min-lein-version "2.8.3"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [amazonica "0.3.136" :exclusions [com.amazonaws/aws-java-sdk
                                                    com.amazonaws/amazon-kinesis-client
                                                    com.amazonaws/dynamodb-streams-kinesis-adapter]]
                  [com.amazonaws/aws-java-sdk-core "1.11.468"]
                  [com.amazonaws/aws-java-sdk-s3 "1.11.468"]
-                 [integrant "0.7.0"]
-                 [org.clojure/clojure "1.9.0"]]
+                 [integrant "0.7.0"]]
+  :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://clojars.org/repo"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
   :profiles
   {:dev {:dependencies [[digest "1.4.8"]]
          :plugins [[jonase/eastwood "0.3.4"]
