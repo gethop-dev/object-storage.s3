a4046938525e1a95c855fa374f5cf6e836218eb0
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-07-05 18:24:45 +0200

    Upgrade dependencies to current versions

--- project.clj
+++ project.clj
@@ -5,11 +5,11 @@
             :url "https://www.mozilla.org/en-US/MPL/2.0/"}
   :min-lein-version "2.8.3"
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [amazonica "0.3.136" :exclusions [com.amazonaws/aws-java-sdk
+                 [amazonica "0.3.143" :exclusions [com.amazonaws/aws-java-sdk
                                                    com.amazonaws/amazon-kinesis-client
                                                    com.amazonaws/dynamodb-streams-kinesis-adapter]]
-                 [com.amazonaws/aws-java-sdk-core "1.11.468"]
-                 [com.amazonaws/aws-java-sdk-s3 "1.11.468"]
+                 [com.amazonaws/aws-java-sdk-core "1.11.586"]
+                 [com.amazonaws/aws-java-sdk-s3 "1.11.586"]
                  [integrant "0.7.0"]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
                                       :username :env/clojars_username
@@ -25,4 +25,4 @@
                    [lein-cljfmt "0.6.2"]]}
    :repl {:repl-options {:host "0.0.0.0"
                          :port 4001}
-          :plugins [[cider/cider-nrepl "0.18.0"]]}})
+          :plugins [[cider/cider-nrepl "0.20.0"]]}})
