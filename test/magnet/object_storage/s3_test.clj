40b195155b19fea158c025003c90b7b6868f74c5
Bingen Galartza <bingen.galartza@magnet.coop>
2019-07-08 13:10:13 +0200

    Add tests for the new functionalities

--- test/magnet/object_storage/s3_test.clj
+++ test/magnet/object_storage/s3_test.clj
@@ -4,14 +4,18 @@
 
 (ns magnet.object-storage.s3-test
   (:require [clojure.test :refer :all]
-            [integrant.core :as ig]
-            [magnet.object-storage.s3]
-            [magnet.object-storage.core :as core]
+            [clojure.java.io :refer [delete-file file input-stream]]
             [digest]
-            [clojure.java.io :refer [delete-file]])
-  (:import [java.util UUID]
-           [com.amazonaws.services.s3.model AmazonS3Exception]
+            [integrant.core :as ig]
+            [magnet.object-storage.core :as core]
+            [magnet.object-storage.s3])
+  (:import [com.amazonaws.services.s3.model AmazonS3Exception]
            [java.io File]
+           [java.io IOException]
+           [java.net URL]
+           [java.security KeyPairGenerator]
+           [java.security SecureRandom]
+           [java.util UUID]
            [magnet.object_storage.s3 AWSS3Bucket]))
 
 (def config {:bucket-name (System/getenv "OBJECT_STORAGE_S3_BUCKET")})
@@ -33,32 +37,99 @@
 
 (use-fixtures :each with-test-files)
 
+(def key-pair
+  (let [kg (KeyPairGenerator/getInstance "RSA")]
+    (.initialize kg 1024 (SecureRandom.))
+    (.generateKeyPair kg)))
+
 (deftest protocol-test
   (let [s3-boundary (ig/init-key :magnet.object-storage/s3 config)]
     (is
      (= (class s3-boundary)
         AWSS3Bucket))))
 
-(deftest ^:integration s3-test
+(deftest ^:integration put-get-file-test
   (let [s3-boundary (ig/init-key :magnet.object-storage/s3 config)
-        new-file-key (str "integration-test-" (UUID/randomUUID))]
-    (is
-     (core/put-object s3-boundary new-file-key (File. test-file-1-path)))
-    (is
-     (core/get-object s3-boundary new-file-key))
-    (core/delete-object s3-boundary new-file-key)
+        file-key (str "integration-test-" (UUID/randomUUID))]
+    (testing "testing put-object"
+      (is (core/put-object s3-boundary file-key (file test-file-1-path))))
+    (testing "testing get-object"
+      (let [result (core/get-object s3-boundary file-key)]
+        (is result)
+        (= (digest/sha-256 (File. test-file-1-path))
+           (digest/sha-256 result))))
+    (core/delete-object s3-boundary file-key)))
+
+(deftest ^:integration put-get-stream-test
+  (let [s3-boundary (ig/init-key :magnet.object-storage/s3 config)
+        file-key (str "integration-test-" (UUID/randomUUID))
+        bytes (.getBytes "Test message")
+        stream (input-stream bytes)]
+    (testing "testing put object stream"
+      (is (core/put-object s3-boundary file-key nil {:input-stream stream
+                                                     :metadata {:content-length (count bytes)}})))
+    (testing "testing get object stream"
+      (let [result (core/get-object s3-boundary file-key)]
+        (is result)
+        (= (digest/sha-256 bytes)
+           (digest/sha-256 result))))
+    (core/delete-object s3-boundary file-key)))
+
+(deftest ^:integration delete-test
+  (let [s3-boundary (ig/init-key :magnet.object-storage/s3 config)
+        file-key (str "integration-test-" (UUID/randomUUID))]
+    (core/put-object s3-boundary file-key (file test-file-1-path))
+    (core/delete-object s3-boundary file-key)
     (is
      (thrown? AmazonS3Exception
-              (core/get-object s3-boundary new-file-key))
+              (core/get-object s3-boundary file-key))
      "Attempt to get an object that's been deleted should throw an exception.")
     (is
      (nil? (core/delete-object s3-boundary (str (UUID/randomUUID))))
-     "Amazonica is expected to allow deletion of a file that doesn't exist.")
-    (let [another-file-key (str "integration-test-" (UUID/randomUUID))]
-      (is
-       (let [file-upload-result (core/put-object s3-boundary another-file-key (File. test-file-1-path))
-             file-2-upload-result (core/put-object s3-boundary another-file-key (File. test-file-2-path))]
-         (= (digest/sha-256 (File. test-file-2-path))
-            (digest/sha-256 (core/get-object s3-boundary another-file-key))))
-       "It should be possible to replace an object.")
-      (core/delete-object s3-boundary another-file-key))))
+     "Amazonica is expected to allow deletion of a file that doesn't exist.")))
+
+(deftest ^:integration replace-object-test
+  (let [s3-boundary (ig/init-key :magnet.object-storage/s3 config)
+        file-key (str "integration-test-" (UUID/randomUUID))
+        file (File. test-file-1-path)
+        file2 (File. test-file-2-path)]
+    (is
+     (let [file-upload-result (core/put-object s3-boundary file-key file)
+           file-2-upload-result (core/put-object s3-boundary file-key file2)]
+       (= (digest/sha-256 (File. test-file-2-path))
+          (digest/sha-256 (core/get-object s3-boundary file-key))))
+     "It should be possible to replace an object.")
+    (core/delete-object s3-boundary file-key)))
+
+(deftest ^:integration presigned-url-test
+  (let [s3-boundary (ig/init-key :magnet.object-storage/s3 config)
+        file-key (str "integration-test-" (UUID/randomUUID))
+        file (file test-file-1-path)]
+    (core/put-object s3-boundary file-key file)
+    (testing "testing default presigned url"
+      (let [result (core/get-object-url s3-boundary file-key)]
+        (is (string? result))
+        (is (URL. result))
+        (is (= (digest/sha-256 file)
+               (digest/sha-256 (slurp result))))))
+    (testing "testing POST presigned url throws exception when using GET"
+      (let [result (core/get-object-url s3-boundary file-key {:method "POST"})]
+        (is (string? result))
+        (is (URL. result))
+        (is (thrown? IOException
+                     (slurp result)))))
+    (core/delete-object s3-boundary file-key)))
+
+(deftest ^:integration encrypted-put-get-test
+  (let [s3-boundary (ig/init-key :magnet.object-storage/s3 config)
+        file-key (str "integration-test-" (UUID/randomUUID))
+        file (file test-file-1-path)]
+    (testing "testing encrypted file put-get"
+        (core/put-object s3-boundary file-key file {:encryption {:key-pair key-pair}})
+      (is (not (=
+                (digest/sha-256 file)
+                (digest/sha-256 (core/get-object s3-boundary file-key)))))
+      (is (=
+           (digest/sha-256 file)
+           (digest/sha-256 (core/get-object s3-boundary file-key {:encryption {:key-pair key-pair}})))))
+    (core/delete-object s3-boundary file-key)))
