ece38dc928120c3a285331deada74b89549bc11f
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2019-10-30 15:49:46 +0100

    Add specs to validate implementations of the protocol
    Instead of having the specs defined on the implementation, define them
    for the core protocol. There are two benefits to this:
    
    - The specs are not tied to a particular implementation, so we avoid
      implementation specific specs that are hard to comply with by other
      implementations.
    - We can reuse this core specs for implementation specific additional
      specs.
    
    This new specs also bring a BREAKING CHANGE to the values returned by
    the procol methods. They all now return a map with at least a
    mandatory boolean key (:success?). They may additionally return other
    keys related to their operation (.e.g., the object we wanted to get
    from the object storage, a url for such object, etc). And in case the
    :success? key is false, there is always a :error-details key that
    holds a map with additional details on the problem the method
    encountered while trying to perform its operation.

--- src/magnet/object_storage/core.clj
+++ src/magnet/object_storage/core.clj
@@ -1,11 +1,45 @@
 ;; This Source Code Form is subject to the terms of the Mozilla Public
 ;; License, v. 2.0. If a copy of the MPL was not distributed with this
 ;; file, You can obtain one at http://mozilla.org/MPL/2.0/
-(ns magnet.object-storage.core)
+
+(ns magnet.object-storage.core
+  (:require [clojure.spec.alpha :as s]))
+
+;; Specs used to validate arguments and return values for
+;; implementations of the protocol
+(s/def ::object-id (s/or :string string? :uuid uuid?))
+(s/def ::encryption map?)
+(s/def ::object (s/or :file #(instance? java.io.File %)
+                      :input-stream #(instance? java.io.InputStream %)))
+(s/def ::success? boolean?)
+(s/def ::error-details map?)
+(s/def ::url (s/or :string string? :url #(instance? java.net.URL %)))
+(s/def ::method #{:create :read :update :delete})
+(s/def ::filename string?)
+(s/def ::object-size (s/and integer? #(>= % 0)))
+(s/def ::metadata (s/keys :req-un [::object-size]))
+
+(s/def ::put-object-opts (s/keys :opt-un [::encryption ::metadata]))
+(s/def ::put-object-args (s/cat :config record? :object-id ::object-id :object ::object :opts ::put-object-opts))
+(s/def ::put-object-ret (s/keys :req-un [::success?]
+                                :opt-un [::error-details]))
+
+(s/def ::get-object-opts (s/keys :opt-un [::encryption]))
+(s/def ::get-object-args (s/cat :config record? :object-id ::object-id :opts (s/? ::get-object-opts)))
+(s/def ::get-object-ret (s/keys :req-un [::success? (or ::object ::error-details)]))
+
+(s/def ::get-object-url-opts (s/keys :opt-un [::method ::filename]))
+(s/def ::get-object-url-args (s/cat :config record? :object-id ::object-id :opts (s/? ::get-object-url-opts)))
+(s/def ::get-object-url-ret (s/keys :req-un [::success? (or ::object-url ::error-details)]))
+
+(s/def ::delete-object-opts map?)
+(s/def ::delete-object-args (s/cat :config record? :object-id ::object-id :opts (s/? ::delete-object-opts)))
+(s/def ::delete-object-ret (s/keys :req-un [::success?]
+                                   :opt-un [::error-details]))
 
 (defprotocol ObjectStorage
   "Abstraction for managing objects storage"
-  (get-object [this obj-id] [this obj-id opts])
-  (get-object-url [this obj-id] [this obj-id opts])
-  (put-object [this obj-id object] [this obj-id object opts])
-  (delete-object [this obj-id]))
+  (put-object [this object-id object] [this object-id object opts])
+  (get-object [this object-id] [this object-id opts])
+  (get-object-url [this object-id] [this object-id opts])
+  (delete-object [this object-id] [this object-id opts]))
