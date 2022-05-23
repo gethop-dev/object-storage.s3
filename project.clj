c24f3bd21f8f252c20ab448f65e61e91d7901e39
spietras <szymon.pietras@magnet.coop>
2022-05-23 11:46:30 +0100

    Update clojars credentials for Github Actions

--- project.clj
+++ project.clj
@@ -13,12 +13,12 @@
                  [integrant "0.7.0"]
                  [magnet/object-storage.core "0.1.3"]]
   :deploy-repositories [["snapshots" {:url "https://clojars.org/repo"
-                                      :username :env/clojars_username
-                                      :password :env/clojars_password
+                                      :username :env/CLOJARS_USERNAME
+                                      :password :env/CLOJARS_PASSWORD
                                       :sign-releases false}]
                         ["releases"  {:url "https://clojars.org/repo"
-                                      :username :env/clojars_username
-                                      :password :env/clojars_password
+                                      :username :env/CLOJARS_USERNAME
+                                      :password :env/CLOJARS_PASSWORD
                                       :sign-releases false}]]
   :profiles
   {:dev [:project/dev :profiles/dev]
