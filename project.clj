087ef1df35ae21cbe8060da5ad2546085b73e11d
Iñaki Arenaza <inaki.arenaza@magnet.coop>
2023-05-07 12:29:51 +0200

    Prepare for next development release.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject dev.gethop/object-storage.s3 "0.7.0"
+(defproject dev.gethop/object-storage.s3 "0.7.1-SNAPSHOT"
   :description "A Duct library for managing AWS S3 objects"
   :url "https://github.com/gethop-dev/object-storage.s3"
   :license {:name "Mozilla Public Licence 2.0"
